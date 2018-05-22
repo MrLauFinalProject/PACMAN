ba1ecccb70e5af87c6fb29fd5f01cb1c656eae07
messifanaj <39036940+messifanaj@users.noreply.github.com>
2018-05-22 19:10:49 -0400

    Update Mover.java

--- Mover.java
+++ Mover.java
@@ -1,56 +1,52 @@
 
-public class Mover
+
+class Mover
 {
-	  /* Framecount is used to count animation frames*/
-	  int frameCount=0;
+  //counts the frames in the animation
+  int frameCount=0;
 
-	  /* State contains the game map */
-	  boolean[][] state;
+  //game map
+  int[][] map;
 
-	  /* gridSize is the size of one square in the game.
-	     max is the height/width of the game.
-	     increment is the speed at which the object moves,
-	     1 increment per move() call */
-	  int gridSize;
-	  int max;
-	  int increment;
+  /* gridSize is the size of one square in the game.
+     max is the height/width of the game.
+     FPS is the speed at which the object moves,
+     1 FPS per move() call */
+  int gridSize;
+  int max;
+  int FPS;
 
-	  /* Generic constructor */
-	  public Mover()
-	  {
-	    gridSize=20;
-	    increment = 4;
-	    max = 400;
-	    state = new boolean[20][20];
-	    for(int i =0;i<20;i++)
-	    {
-	      for(int j=0;j<20;j++)
-	      {
-	        state[i][j] = false;
-	      }
-	    }
-	  }
-	  /* Updates the state information */
-	  public void updateState(boolean[][] state)
-	  {
-	    for(int i =0;i<20;i++)
-	    {
-	      for(int j=0;j<20;j++)
-	      {
-	        this.state[i][j] = state[i][j];
-	      }
-	    }
-	  }
+  /* Generic constructor */
+  public Mover()
+  {
+    gridSize=20;
+    FPS = 4;
+    max = 400;
+    map = new int[20][20];
+    for(int i =0;i<20;i++){
+      for(int j=0;j<20;j++){
+        map[i][j] = 0;
+      }
+    }
+  }
 
-	  /* Determines if a set of coordinates is a valid destination.*/
-	  public boolean isValidDest(int x, int y)
-	  {
-	    /* The first statements check that the x and y are inbounds.  The last statement checks the map to
-	       see if it's a valid location */
-	    if ((((x)%20==0) || ((y)%20)==0) && 20<=x && x<400 && 20<= y && y<400 && state[x/20-1][y/20-1] )
-	    {
-	      return true;
-	    }
-	    return false;
-	  } 
-	}
+  /* Updates the map information */
+  public void updatemap(int[][] map)
+  {
+    for(int i =0;i<20;i++){
+      for(int j=0;j<20;j++){
+        this.map[i][j] = map[i][j];
+      }
+    }
+  }
+
+  /* Determines if a set of coordinates is a valid destination.*/
+  public boolean isValidDest(int x, int y)
+  {
+    // is the new location within play area and not a wall
+    if ((((x)%20==0) || ((y)%20)==0) && 20<=x && x<400 && 20<= y && y<400 && (map[x/20-1][y/20-1]) != 0 ){
+      return true;
+    }
+    return false;
+  } 
+}
